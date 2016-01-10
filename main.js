076e31bb3598e272671f48310eeb81c2f604131e
takeyuichi <y.takey@gmail.com>
2016-01-10 16:20:12 +0900

    Init electron files

---
+++ main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// アプリケーションをコントロールするモジュール
+var app = require('app');
+// ウィンドウを作成するモジュール
+var BrowserWindow = require('browser-window');
+
+// メインウィンドウはGCされないようにグローバル宣言
+var mainWindow = null;
+
+// 全てのウィンドウが閉じたら終了
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面の表示。ウィンドウの幅、高さを指定できる
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  });
+});
