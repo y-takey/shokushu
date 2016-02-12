d652a674c03c49b067e9f92ef736e1ad6daba02c
takeyuichi <y.takey@gmail.com>
2016-02-12 21:44:30 +0900

    Feature keyboard friendly

--- app/components/Viewer.js
+++ app/components/Viewer.js
@@ -11,6 +11,18 @@ import FloatingActionButton from 'material-ui/lib/floating-action-button';
 import { Link } from 'react-router';
 import styles from './Viewer.module.css';
 
+// Constants
+const Keys = {
+  ENTER: 13,
+  LEFT_ARROW: 37,
+  UP_ARROW: 38,
+  RIGHT_ARROW: 39,
+  DOWN_ARROW: 40,
+  SPACE: 32
+};
+
+const STEP = 10;
+
 class Viewer extends Component {
   static propTypes = {
     increment: PropTypes.func.isRequired,
@@ -21,12 +33,18 @@ class Viewer extends Component {
 
   constructor(props, context) {
     super(props, context)
-    this.state = { showBar: true }
-    _.bindAll(this, 'onMouseMove', 'hideActionBar')
+    this.state = { showBar: true, playing: false }
+    _.bindAll(this, 'onMouseMove', 'hideActionBar', 'stepBackward', 'play', 'stop', 'stepForward', 'handleKeyDown')
   }
 
   componentDidMount() {
+    document.addEventListener('keydown', this.handleKeyDown);
     this.hideActionBar();
+    this.state = { showBar: true, playing: false }
+  }
+
+  componentWillUnMount() {
+    document.removeEventListener('keydown', this.handleKeyDown);
   }
 
   onMouseMove() {
@@ -44,25 +62,60 @@ class Viewer extends Component {
   }
 
   stepBackward() {
-    console.log("clicked stepBackward");
+    console.log("clicked stepBackward now:", this.refs.video.currentTime);
+    this.refs.video.currentTime -= STEP
   };
 
   play() {
     console.log("clicked play");
     this.refs.video.play();
+    this.setState({ playing: true })
   };
 
   stop() {
     console.log("clicked stop");
     this.refs.video.pause();
+    this.setState({ playing: false })
   };
 
   stepForward() {
-    console.log("clicked stepForward");
+    console.log("clicked stepForward now:", this.refs.video.currentTime);
+    this.refs.video.currentTime += STEP
   };
 
   fullscreen() {
     this.refs.video.webkitRequestFullscreen();
+  }
+
+  handleKeyDown(e) {
+    console.log("-- key is :", e.keyCode)
+    switch (e.keyCode) {
+      case Keys.LEFT_ARROW:
+        e.preventDefault();
+        this.stepBackward();
+        console.log("downed key is LEFT_ARROW");
+        return;
+      case Keys.RIGHT_ARROW:
+        e.preventDefault();
+        this.stepForward();
+        console.log("downed key is RIGHT_ARROW");
+        return;
+      case Keys.UP_ARROW:
+        e.preventDefault();
+        console.log("downed key is UP_ARROW");
+        return;
+      case Keys.DOWN_ARROW:
+        e.preventDefault();
+        console.log("downed key is DOWN_ARROW");
+        return;
+      case Keys.ENTER:
+        e.preventDefault();
+        this.state.playing ? this.stop() : this.play();
+        console.log("downed key is ENTER");
+        return;
+      default:
+        console.log("other key is downed:", e.keyCode)
+    }
   }
 
   render() {
@@ -91,8 +144,7 @@ class Viewer extends Component {
           <CardMedia overlayStyle={ { display: (this.state.showBar ? 'block' : 'none') } }
             overlay={[
               btn("step-backward", this.stepBackward),
-              btn("play", this.play),
-              btn("stop", this.stop),
+              (this.state.playing ? btn("stop", this.stop) : btn("play", this.play)),
               btn("step-forward", this.stepForward),
               btn("expand", this.fullscreen)
             ]}
