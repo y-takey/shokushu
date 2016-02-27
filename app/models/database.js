c36c8a55eb77e696d01400508604e2a254c4b006
takeyuichi <y.takey@gmail.com>
2016-02-27 19:32:18 +0900

    Fix reference correct database file's path

--- app/models/database.js
+++ app/models/database.js
@@ -1,6 +1,9 @@
+const remote = require('remote');
+const app = remote.require('electron').app;
+import path from 'path';
 import low from 'lowdb';
 import storage from 'lowdb/file-sync';
-const dbFilename = "./db.json"
+const dbFilename = path.join(app.getAppPath(), 'db.json');
 const db = low(dbFilename, { storage });
 
 export { db, dbFilename };
